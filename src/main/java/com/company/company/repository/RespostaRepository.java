215faa20df3c48c2643aaa81473f678e8eaea37f
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2025-06-14 01:49:53 -0300

    create api to return the titles to fill the combo

--- src/main/java/com/company/company/repository/RespostaRepository.java
+++ src/main/java/com/company/company/repository/RespostaRepository.java
@@ -13,4 +13,5 @@ public interface RespostaRepository extends MongoRepository<Resposta, String> {
 
     @Query(value = "{funcionario:?0}")
     List<Resposta> findByRespostaByFuncionario(Funcionario funcionario);
+
 }
--- src/main/java/com/company/company/repository/TituloRepository.java
+++ src/main/java/com/company/company/repository/TituloRepository.java
@@ -4,7 +4,8 @@ import com.company.company.model.Titulo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TituloRepository extends MongoRepository<Titulo, String> {
-
 }
